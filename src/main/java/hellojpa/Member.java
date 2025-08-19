6867274234c9f1e3b9ac49ce6abaa3d12549049e
leedy5521 <leedy5521@naver.com>
2025-08-20 00:36:12 +0900

    다대다 한계극(연결 테이블용 엔티티 추가)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -27,9 +27,8 @@ public class Member {
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
-    @ManyToMany
-    @JoinTable(name = "MEMBER_PRODUCT")
-    private List<Product> products = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> MemberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;

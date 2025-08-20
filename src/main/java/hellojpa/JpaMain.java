bb4f4d2965d91e91873d29e61c5a77ab8e86d52e
leedy5521 <leedy5521@naver.com>
2025-08-20 16:40:12 +0900

    상속관계 매핑(SINGLE_TABLE 전략)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,16 +15,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
+            Movie movie = new Movie();
+            movie.setDirector("aaa");
+            movie.setActor("bbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
 
-            em.persist(member);
+            em.persist(movie);
 
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
+            em.flush();
+            em.clear();
 
-            em.persist(team);
+            Movie findmovie = em.find(Movie.class, movie.getId());
+            System.out.println("findmovie = " + findmovie);
+
 
             tx.commit();
         }catch (Exception e) {
